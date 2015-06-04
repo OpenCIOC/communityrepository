a91fe5a0b796cce143cb198a63ce772f689a7340
Christopher Lambacher <chris@kateandchris.net>
2015-06-03 23:53:59 -0400

    License header for external systems files

--- python/communitymanager/views/externalsystem.py
+++ python/communitymanager/views/externalsystem.py
@@ -1,24 +1,30 @@
-# =================================================================
-# Copyright (C) 2011 Community Information Online Consortium (CIOC)
-# http://www.cioc.ca
-# Developed By Katherine Lambacher / KCL Custom Software
-# If you did not receive a copy of the license agreement with this
-# software, please contact CIOC via their website above.
-# =================================================================
+# =========================================================================================
+#  Copyright 2015 Community Information Online Consortium (CIOC) and KCL Software Solutions
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+# =========================================================================================
+
 # std lib
-# from xml.etree import ElementTree as ET
 
 # 3rd party
-# from formencode.variabledecode import variable_decode
 from pyramid.httpexceptions import HTTPNotFound, HTTPFound
 from pyramid.view import view_config, view_defaults
-from pyramid.security import Allow, DENY_ALL  # , NO_PERMISSION_REQUIRED, ALL_PERMISSIONS
+from pyramid.security import Allow, DENY_ALL
 from markupsafe import Markup
 
 # this app
-from communitymanager.lib import validators  # , security
-from communitymanager.views.base import ViewBase  # , xml_to_dict_list
-# from communitymanager.lib.request import get_translate_fn
+from communitymanager.lib import validators
+from communitymanager.views.base import ViewBase
 
 
 import logging
