e80f966cd19258673d493eed71a2f65971e7ec81
Christopher Lambacher <chris@kateandchris.net>
2015-06-30 13:23:21 -0400

    Finalization of mapping file format

--- python/sql/Stored Procedures/dbo.sp_External_Community_l_xml.sql
+++ python/sql/Stored Procedures/dbo.sp_External_Community_l_xml.sql
@@ -1,3 +1,4 @@
+
 SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
@@ -9,37 +10,45 @@ CREATE PROCEDURE [dbo].[sp_External_Community_l_xml]
 	@SystemCode varchar(30)
 AS
 BEGIN
-	SET NOCOUNT ON
 
-	SELECT	
+SET NOCOUNT ON
+
+SELECT	
 	(SELECT
-	excm.EXT_ID AS [@id],
-			excm.AreaName AS [@AreaName],
-		    excm.ExternalID AS [@ExternalID],
-			excm.PrimaryAreaType AS [@PrimaryAreaType],
-			excm.SubAreaType AS [@SubAreaType],
-			psc.NameOrCode AS [@Province],
-			psc.Country AS [@Country],
-			excm.AIRSExportType AS [@AIRSExportType],
-			cm.CM_GUID AS [@MappedGUID],
-			excm2.AreaName AS [@ParentName],
-			excm2.ExternalID AS [@ParentExternalID]
+		excm.EXT_ID AS [@ID],
+		excm.EXT_GUID AS [@GUID],
+		excm.AreaName AS [@AreaName],
+		excm.ExternalID AS [@ExternalID],
+		excm.PrimaryAreaType AS [@PrimaryAreaType],
+		excm.SubAreaType AS [@SubAreaType],
+		psc.NameOrCode AS [@Province],
+		psc.Country AS [@Country],
+		excm.AIRSExportType AS [@AIRSExportType],
+		excm.Parent_ID AS [@Parent_ID]
 	FOR XML PATH('MapEntry')) AS row
-	FROM External_Community excm
-	LEFT JOIN External_Community excm2
-		ON excm2.EXT_ID=excm.Parent_ID
-	LEFT JOIN Community_Type pat
-		ON pat.Code = excm.PrimaryAreaType
-	LEFT JOIN Community_Type_Name patn
-		ON patn.Code = pat.Code AND patn.LangID=(SELECT TOP 1 LangID FROM Community_Type_Name WHERE pat.Code=Code ORDER BY CASE WHEN LangID=@@LANGID THEN 0 ELSE 1 END, LangID)
-	LEFT JOIN ProvinceState psc
-		ON excm.ProvinceState=psc.ProvID
-	LEFT JOIN Community cm
-		ON cm.CM_ID = excm.CM_ID
-	WHERE excm.SystemCode=@SystemCode
+FROM External_Community excm
+LEFT JOIN Community_Type pat
+	ON pat.Code = excm.PrimaryAreaType
+LEFT JOIN Community_Type_Name patn
+	ON patn.Code = pat.Code AND patn.LangID=(SELECT TOP 1 LangID FROM Community_Type_Name WHERE pat.Code=Code ORDER BY CASE WHEN LangID=@@LANGID THEN 0 ELSE 1 END, LangID)
+LEFT JOIN ProvinceState psc
+	ON excm.ProvinceState=psc.ProvID
+LEFT JOIN Community cm
+	ON cm.CM_ID = excm.CM_ID
+WHERE excm.SystemCode=@SystemCode
 
+SELECT
+	(SELECT
+		[@GUID] = cm.CM_GUID,
+		[@MapOne_EXT_ID] = cem.MapOneEXTID,
+		[MapAll] = cem.MapAllEXTID
+		FOR XML PATH('Community'), TYPE)
+	FROM Community cm
+	INNER JOIN dbo.Community_External_Map cem ON cem.CM_ID = cm.CM_ID
+	ORDER BY cm.SortCode
 
-	SET NOCOUNT OFF
+SET NOCOUNT OFF
+
 END
 
 
@@ -48,5 +57,6 @@ END
 
 
 GO
+
 GRANT EXECUTE ON  [dbo].[sp_External_Community_l_xml] TO [web_user]
 GO
