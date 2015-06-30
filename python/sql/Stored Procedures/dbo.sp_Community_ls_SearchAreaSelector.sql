33cc68976e1bfe6cb22f13e623205d48abd7162e
Christopher Lambacher <chris@kateandchris.net>
2015-06-30 13:22:58 -0400

    Community disambiguation

--- python/sql/Stored Procedures/dbo.sp_Community_ls_SearchAreaSelector.sql
+++ python/sql/Stored Procedures/dbo.sp_Community_ls_SearchAreaSelector.sql
@@ -1,3 +1,4 @@
+
 SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
@@ -13,7 +14,7 @@ AS BEGIN
 
 SET NOCOUNT ON
 
-SELECT	cm.CM_ID, 
+SELECT	DISTINCT cm.CM_ID, 
 		cmn.Name,
 		cmn.Name
 			+ CASE WHEN cmn.Name LIKE '%' + @searchStr + '%' THEN '' ELSE ' [' + anm.AltName + ']' END
@@ -58,5 +59,6 @@ SET NOCOUNT OFF
 END
 
 GO
+
 GRANT EXECUTE ON  [dbo].[sp_Community_ls_SearchAreaSelector] TO [web_user]
 GO
