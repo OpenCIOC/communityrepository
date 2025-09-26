90f3419a52ee3836411c4d0cdc32c9edf49caf51
Chris Lambacher <chris@kateadchris.net>
2025-09-26 18:16:22 -0400

    Fix Raw vs Safe Config parser

--- python/communitymanager/lib/config.py
+++ python/communitymanager/lib/config.py
@@ -19,43 +19,43 @@ from configparser import RawConfigParser as ConfigParser
 
 
 class ConfigManager(object):
-	def __init__(self, config_file):
-		self._config_file = config_file
-		self.load()
+    def __init__(self, config_file):
+        self._config_file = config_file
+        self.load()
 
-	def load(self):
-		self._changed = os.path.getmtime(self._config_file)
+    def load(self):
+        self._changed = os.path.getmtime(self._config_file)
 
-		cp = ConfigParser()
-		cp.read(self._config_file)
+        cp = ConfigParser()
+        cp.read(self._config_file)
 
-		self.config_dict = dict(cp.items('global'))
+        self.config_dict = dict(cp.items("global"))
 
-	def maybe_reload(self, config_file=None):
-		if config_file and config_file != self._config_file:
-			self._config_file = config_file
-			self.load()
-			return
+    def maybe_reload(self, config_file=None):
+        if config_file and config_file != self._config_file:
+            self._config_file = config_file
+            self.load()
+            return
 
-		mtime = os.path.getmtime(self._config_file)
-		if self._changed != mtime:
-			self.load()
+        mtime = os.path.getmtime(self._config_file)
+        if self._changed != mtime:
+            self.load()
 
 
 _config = None
 
 
 def get_config(config_file, include_changed=False):
-	global _config
-	if not _config:
-		_config = ConfigManager(config_file)
-		changed = True
-	else:
-		before = _config._changed
-		_config.maybe_reload(config_file)
-		changed = _config._changed != before
+    global _config
+    if not _config:
+        _config = ConfigManager(config_file)
+        changed = True
+    else:
+        before = _config._changed
+        _config.maybe_reload(config_file)
+        changed = _config._changed != before
 
-	if include_changed:
-		return (_config.config_dict.copy(), changed)
+    if include_changed:
+        return (_config.config_dict.copy(), changed)
 
-	return _config.config_dict.copy()
+    return _config.config_dict.copy()
